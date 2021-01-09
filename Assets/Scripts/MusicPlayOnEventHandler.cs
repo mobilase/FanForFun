83745b8efd09592c16b4fb1413d5ddba179b3a91
Lavrov Sergey <lavrov-sergey@yandex.ru>
2021-01-09 07:28:12 +0300

    Проект обновлен до Unity 2019.4 и решены проблемы интеграции с Git

--- Assets/Scripts/MusicPlayOnEventHandler.cs
+++ Assets/Scripts/MusicPlayOnEventHandler.cs
@@ -53,7 +53,7 @@ public class MusicPlayOnEventHandler : MonoBehaviour, ITrackableEventHandler
             OnTrackingFound();
         }
         else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
-                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
         {
             Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
             OnTrackingLost();
